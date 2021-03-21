0520209712290df87fc9d8abdfed86d049bb6ea4
Mauriciow <marcos_c79@hotmail.com>
2021-03-20 19:00:23 -0600

    conf to save field 'name' into DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    # all code below is to save the 'name' field in the register_form in the User model 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
 end
